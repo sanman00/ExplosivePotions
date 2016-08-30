8fe3b50a2104769196230b8ba68e6f8a4fb4f1fb
sanman00 <sanjaywygold2011@hotmail.co.uk>
2016-08-30 16:50:09 +0100

    Remove damage value-based tests

--- src/main/java/com/comze/sanman00/mods/minecraft/expotions/proxy/CommonProxy.java
+++ src/main/java/com/comze/sanman00/mods/minecraft/expotions/proxy/CommonProxy.java
@@ -1,8 +1,5 @@
 package com.comze.sanman00.mods.minecraft.expotions.proxy;
 
-import java.lang.reflect.Field;
-import java.util.List;
-import org.apache.logging.log4j.Level;
 import com.comze.sanman00.mods.minecraft.expotions.Main;
 import com.comze.sanman00.mods.minecraft.expotions.entity.EntityExplosivePotion;
 import com.comze.sanman00.mods.minecraft.expotions.item.ItemExplosivePotion;
@@ -11,10 +8,6 @@ import com.comze.sanman00.mods.minecraft.expotions.util.WaterBottleOnlyInputBrew
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTTagString;
 import net.minecraftforge.common.brewing.BrewingRecipe;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -32,47 +25,9 @@ public class CommonProxy {
         GameRegistry.register(ItemExplosivePotion.instance);
         GameRegistry.register(ItemThrowableExplosivePotion.instance);
         EntityRegistry.registerModEntity(EntityExplosivePotion.class, "ThrowableExplosivePotion", 690, Main.instance, 2, 5, true);
-        BrewingRecipeRegistry.addRecipe(new WaterBottleOnlyInputBrewingRecipe(new ItemStack(Items.POTIONITEM), new ItemStack(Blocks.TNT), new ItemStack(ItemExplosivePotion.instance)) {
-            @Override
-            public ItemStack getOutput() {
-                ItemStack outputStack = super.getOutput();
-                NBTTagCompound displayCompound = outputStack.getSubCompound("display", true);
-                NBTTagList lore = displayCompound.getTagList("Lore", 8);
-                lore.appendTag(new NBTTagString("Strength: " + outputStack.getItemDamage()));
-                displayCompound.setTag("Lore", lore);
-                return outputStack;
-            }
-        });
-        BrewingRecipeRegistry.addRecipe(new BrewingRecipe(new ItemStack(ItemExplosivePotion.instance), new ItemStack(Blocks.TNT), new ItemStack(ItemExplosivePotion.instance)) {
-            @Override
-            public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
-                ItemStack outputStack = new ItemStack(ItemExplosivePotion.instance, 1, input.getItemDamage() + 1);
-                NBTTagCompound displayCompound = outputStack.getSubCompound("display", true);
-                NBTTagList lore = displayCompound.getTagList("Lore", 8);
-                applyStrengthLore(lore, outputStack.getItemDamage());
-                displayCompound.setTag("Lore", lore);
-                return outputStack;
-            }
-        });
+        BrewingRecipeRegistry.addRecipe(new WaterBottleOnlyInputBrewingRecipe(new ItemStack(Items.POTIONITEM), new ItemStack(Blocks.TNT), new ItemStack(ItemExplosivePotion.instance)));
+        BrewingRecipeRegistry.addRecipe(new BrewingRecipe(new ItemStack(ItemExplosivePotion.instance), new ItemStack(Blocks.TNT), new ItemStack(ItemExplosivePotion.instance)));
         BrewingRecipeRegistry.addRecipe(new ItemStack(ItemExplosivePotion.instance), new ItemStack(Items.GUNPOWDER), new ItemStack(ItemThrowableExplosivePotion.instance));
-    }
-    
-    private static void applyStrengthLore(NBTTagList lore, int strength) {
-        //replace with AT?
-        try {
-            Field tagListField = lore.getClass().getDeclaredField("tagList");
-            tagListField.setAccessible(true);
-            @SuppressWarnings("unchecked")
-            List<NBTBase> tagList = (List<NBTBase>) tagListField.get(lore);
-            tagList.clear();
-            tagListField.set(lore, tagList);
-            lore.appendTag(new NBTTagString("Strength: " + strength));
-        }
-        
-        catch (NoSuchFieldException | IllegalAccessException e) {
-            Main.getLogger().log(Level.ERROR, "Could not access tagList field, unable to apply strength lore");
-            e.printStackTrace();
-        }
     }
 
     public void init(FMLInitializationEvent event) {
