17f9e83c851fc0f4a7eb8c24923efce9a29b0fc3
sanman00 <sanjaywygold2011@hotmail.co.uk>
2020-02-14 18:35:23 +0000

    Allow spicy explosive potion entity to be spawned from a dispenser

--- src/main/java/com/comze/sanman00/mods/minecraft/expotions/entity/DispenseBehaviourExplosivePotion.java
+++ src/main/java/com/comze/sanman00/mods/minecraft/expotions/entity/DispenseBehaviourExplosivePotion.java
@@ -1,5 +1,6 @@
 package com.comze.sanman00.mods.minecraft.expotions.entity;
 
+import com.comze.sanman00.mods.minecraft.expotions.item.ItemSpicyThrowableExplosivePotion;
 import com.comze.sanman00.mods.minecraft.expotions.util.ItemUtil;
 import net.minecraft.dispenser.BehaviorProjectileDispense;
 import net.minecraft.dispenser.IPosition;
@@ -11,7 +12,12 @@ public class DispenseBehaviourExplosivePotion extends BehaviorProjectileDispense
 
     @Override
     protected IProjectile getProjectileEntity(World world, IPosition position, ItemStack stack) {
-        EntityExplosivePotion potion = new EntityExplosivePotion(world, position.getX(), position.getY(), position.getZ());
+        boolean spicyItem = stack.getItem() == ItemSpicyThrowableExplosivePotion.INSTANCE;
+        
+        double x = position.getX();
+        double y = position.getY();
+        double z = position.getZ();
+        EntityExplosivePotion potion = spicyItem ? new EntitySpicyExplosivePotion(world, x, y, z) : new EntityExplosivePotion(world, x, y, z);
         potion.setStrength(ItemUtil.getStrength(stack));
         return potion;
     }
